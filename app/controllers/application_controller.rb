5f12fd86b951aa0172b364fdb6721467b6a8cd71
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-04-15 10:19:52 +0300

    api fix 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,31 +13,31 @@ class ApplicationController < Sinatra::Base
     # Create a new blog
     blog = Blog.create(params)
     if blog.valid?
-      blog.to_json # Return blog data as JSON
+      blog.to_json 
     else
-      { error: 'Failed to create blog' }.to_json # Return error message as JSON
+      { error: 'Failed to create blog' }.to_json 
     end
   end
   
   get '/blogs' do
     # Get all blogs
     blogs = Blog.all
-    blogs.to_json # Return blogs data as JSON
+    blogs.to_json 
   end
   
   get '/blogs/:id' do
     # Get a specific blog
     blog = Blog.find(params[:id])
-    blog.to_json # Return blog data as JSON
+    blog.to_json 
   end
   
   patch '/blogs/:id' do
     # Update a blog
     blog = Blog.find(params[:id])
     if blog.update(params)
-      blog.to_json # Return updated blog data as JSON
+      blog.to_json 
     else
-      { error: 'Failed to update blog' }.to_json # Return error message as JSON
+      { error: 'Failed to update blog' }.to_json 
     end
   end
   
@@ -45,9 +45,11 @@ class ApplicationController < Sinatra::Base
     # Delete a blog
     blog = Blog.find(params[:id])
     if blog.destroy
-      { message: 'Blog deleted successfully' }.to_json # Return success message as JSON
+      content_type :json 
+      { message: 'Blog deleted successfully' }.to_json 
     else
-      { error: 'Failed to delete blog' }.to_json # Return error message as JSON
+      content_type :json 
+      { error: 'Failed to delete blog' }.to_json 
     end
   end
 end
